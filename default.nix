3da7cf56cab64ec6b1eca9ffaccbeeaef0300139
brest computer club <brest-computer-club@protonmail.com>
2020-09-21 15:59:39 +0200

    add nix & gitignore

---
+++ default.nix
@@ -0,0 +1,17 @@
+let                                
+  pkgs = import (builtins.fetchTarball {                                
+    name = "nixos-20.03";                                
+    url = "https://github.com/NixOS/nixpkgs/archive/20.03.tar.gz";                                
+    sha256 = "0182ys095dfx02vl2a20j1hz92dx3mfgz2a6fhn31bqlp1wa8hlq";                                
+  }) {};                                
+  src = builtins.fetchGit {                                
+    url = "https://github.com/brest-computer-club/nix";                                
+    ref = "master";                                
+  };                                
+  altPkgs = import src pkgs;                                
+in                                
+pkgs.mkShell {                                
+  buildInputs = with pkgs;                                
+     altPkgs.bundles.elm pkgs ++                                
+     [ pkgs.nodePackages.uglify-js];                                
+}      
