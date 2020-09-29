69c3a372029ffabc37ad0dbdfa099b55de80b1aa
brest computer club <brest-computer-club@protonmail.com>
2020-09-29 20:42:48 +0200

    simple elm architecture

--- src/Main.elm
+++ src/Main.elm
@@ -2,6 +2,7 @@ module Main exposing (main)
 
 import Browser
 import Html exposing (Html, div, h1, text)
+import Html.Events as HE
 
 
 type alias Model =
@@ -13,23 +14,45 @@ init =
     0
 
 
+type Msg
+    = Inc
+    | Dec
+    | IncBy Int
+
+
 titleText : String
 titleText =
     "== type writer =="
 
 
-view : Model -> Html a
+view : Model -> Html Msg
 view m =
     div []
-        [ h1 [] [ text titleText ]
+        [ h1 [ HE.onClick Inc ] [ text titleText ]
+        , Html.button [ HE.onClick Inc ] [ text "inc" ]
+        , Html.button [ HE.onClick Dec ] [ text "dec" ]
+        , Html.button [ HE.onClick <| IncBy 100 ] [ text "inc 100" ]
         , text (String.fromInt m)
         ]
 
 
-main : Program () Model a
+update : Msg -> Model -> Model
+update msg m =
+    case msg of
+        Inc ->
+            m + 1
+
+        Dec ->
+            m - 1
+
+        IncBy x ->
+            m + x
+
+
+main : Program () Model Msg
 main =
     Browser.sandbox
         { init = init
         , view = view
-        , update = \_ _ -> 34
+        , update = update
         }
