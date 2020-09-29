2b71f9cd82d9b254ec7f3941da29274e56e32043
brest computer club <brest-computer-club@protonmail.com>
2020-09-29 20:20:07 +0200

    string operations

--- src/Main.elm
+++ src/Main.elm
@@ -4,10 +4,20 @@ import Browser
 import Html
 
 
-main : Program () () msg
+titleText : String
+titleText =
+    (\str -> String.repeat 4 str |> String.reverse |> String.toUpper) "hello"
+
+
+title : Html.Html a
+title =
+    Html.h1 [] [ Html.text titleText ]
+
+
+main : Program () () a
 main =
     Browser.sandbox
         { init = ()
-        , view = \_ -> Html.text "salut"
+        , view = \_ -> title
         , update = \_ _ -> ()
         }
